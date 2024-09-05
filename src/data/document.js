40b4c7ce3e2357df5476e63cbb44c788c9f8e5fa
Aniket Yadav <aniketyadav690@gmail.com>
2024-09-05 22:53:05 +0530

    update: data and assets folder

---
+++ src/data/document.js
@@ -0,0 +1,38 @@
+import cat1 from "../assets/cat1.jpg";
+import cat2 from "../assets/cat2.jpg";
+import cat3 from "../assets/cat3.jpg";
+import cat4 from "../assets/cat4.jpg";
+import cat5 from "../assets/cat5.jpg";
+
+export const documents = [
+  {
+    type: "bankdraft",
+    title: "Bank Draft",
+    position: 0,
+    image: cat1,
+  },
+  {
+    type: "bill-of-lading",
+    title: "Bill of Lading",
+    position: 1,
+    image: cat2,
+  },
+  {
+    type: "invoice",
+    title: "Invoice",
+    position: 2,
+    image: cat3,
+  },
+  {
+    type: "bank-draft-2",
+    title: "Bank Draft 2",
+    position: 3,
+    image: cat4,
+  },
+  {
+    type: "bill-of-lading-2",
+    title: "Bill of Lading 2",
+    position: 4,
+    image: cat5,
+  },
+];
